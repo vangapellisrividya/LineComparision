c69751e31a9194a1b8b4e9f384167aae704e43f3
vangapellisrividyam <vangapellisreevidya@gmail.com>
2021-08-01 11:42:49 +0530

    add:euality of lengths

--- src/com/bl/linecomparision/LineComparision.java
+++ src/com/bl/linecomparision/LineComparision.java
@@ -2,11 +2,28 @@ package com.bl.linecomparision;
 
 public class LineComparision {
 	public static void main(String[] args) {
-		int x1,x2,y1,y2;
-		x1=1;x2=2;y1=1;y2=2;
-		double linelength;
-		linelength = Math.sqrt((x2-x1)*(x2-x1) + (y2-y1)*(y2-y1));
-		System.out.println("length of line "+linelength);
-	}
+		int x1, x2, y1, y2;
+		int x3 = 1, x4 = 2, y3 = 1, y4 = 2;
+		x1 = 1;
+		x2 = 2;
+		y1 = 1;
+		y2 = 2;
+		double linelength1,linelength2;
+		linelength2 = Math.sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1));
+		System.out.println("length of line " + linelength2);
+		linelength1 = Math.sqrt((x4 - x3) * (x4 - x3) + (y4 - y3) * (y4 - y3));
+		System.out.println("length of line " + linelength1);
+		if (linelength1 == linelength2)
+		{
+			System.out.println("length is equal ");
+		} else 
+		{
+			System.out.println("length is not equal");
+		}
 
+	}
 }
+
+
+
+
